d01dcccc7de63bc723ff554cf6416cc9d0df6e04
Favour Aniogor <faniogor@gmail.com>
2023-09-24 00:14:48 +0100

    This will a little styling

--- components/widget.jsx
+++ components/widget.jsx
@@ -2,42 +2,111 @@ let greeting = "Have a great day";
 
 return (
   <>
-    <div class="container border border-info p-3 d-flex align-items-center">
-      <div class="container border rounded">
-        <div class="d-flex align-items-center justify-content-start">
-          <button class="btn btn-primary">Swap</button>
-          <button class="btn btn-primary">Bridge</button>
-          <p className="justify-self-end text-right">Near</p>
+    <div className="container border border-info p-3 d-flex align-items-center">
+      <div
+        className="container rounded d-flex flex-column align-items-center justify-content-center gap-2 p-3"
+        style={{ maxWidth: "300px", boxShadow: "0px 0px 10px #0000001A" }}
+      >
+        <div className="d-flex align-items-center justify-content-start w-100 gap-2">
+          <p className="fw-bold p-0 m-0">Swap</p>
+          <p className="fw-bold p-0 m-0">Bridge</p>
+          <p className="p-0 m-0">Near</p>
         </div>
-        <div>
+        <div
+          className="d-flex flex-column align-items-center justify-content-center gap-2"
+          style={{ position: "relative", width: "100%" }}
+        >
           {/* {First Box} */}
-          <div class="container border rounded bg-gray d-flex">
-            <div class="container">
-              <p>Send</p>
-              <p>120</p>
+          <div
+            className="container border rounded d-flex p-0"
+            style={{ backgroundColor: "#F0F0F0F0", width: "100%" }}
+          >
+            <div className="container w-50 p-2">
+              <p className="p-0 m-0" style={{ fontSize: "0.7rem" }}>
+                Send
+              </p>
+              <input
+                className="fw-bold fs-3 max-w-full"
+                style={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                  outline: "none",
+                  boxShadow: "none",
+                }}
+                type="text"
+                inputMode="numeric"
+                min={0}
+                placeholder="0"
+                onFocus={(e) => {
+                  e.target.border = "none";
+                }}
+              />
             </div>
-            <div class="container border border-radius-3">
-              <p>120.54</p>
+            <div className="container w-50 p-2">
+              <p
+                className="w-100"
+                style={{ fontSize: "0.7rem", textAlign: "right" }}
+              >
+                $120.54
+              </p>
               <div></div>
             </div>
           </div>
           {/* {Second Box} */}
-          <div class="container border border-radius-3">
-            <div class="container border border-radius-3">
-              <p>Send</p>
-              <p>120.54</p>
+          <div
+            className="container border rounded d-flex p-0"
+            style={{ backgroundColor: "#F0F0F0F0", width: "100%" }}
+          >
+            <div className="container w-50 p-2">
+              <p className="p-0 m-0" style={{ fontSize: "0.7rem" }}>
+                Receive
+              </p>
+              <input
+                className="fw-bold fs-3 max-w-full"
+                style={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                  outline: "none",
+                  boxShadow: "none",
+                  width: "fit-content",
+                }}
+                type="text"
+                inputMode="numeric"
+                min={0}
+                placeholder="0"
+                onFocus={(e) => {
+                  e.target.border = "none";
+                }}
+              />
             </div>
-            <div class="container border border-radius-3">
-              <p>120</p>
+            <div className="container border border-radius-3 w-50 p-2">
+              <p
+                className="text-right w-100"
+                style={{ fontSize: "0.7rem", textAlign: "right" }}
+              >
+                $120.54
+              </p>
               <div></div>
             </div>
           </div>
           {/* {Swith Button} */}
-          <button class="btn btn-primary">Switch</button>
+          <button
+            className="btn fs-2"
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: "37%",
+              background: "transparent",
+            }}
+          >
+            🔃
+          </button>
         </div>
 
         {/* {Swap/Bridge Button} */}
-        <button class="btn btn-primary">Swap</button>
+        <button className="btn btn-primary fs-bold w-100" type="button">
+          Swap
+        </button>
       </div>
     </div>
   </>
