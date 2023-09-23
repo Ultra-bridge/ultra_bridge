2e57569479aa0dced4a59487648ad558a9de1a86
Favour Aniogor <faniogor@gmail.com>
2023-09-23 16:17:44 +0100

    This is to Initialise the Ultra Bridge Project

---
+++ components/widget.jsx
@@ -0,0 +1,10 @@
+let greeting = "Have a great day";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center min-vw-100">
+      <h1>Hello</h1>
+      <p> {greeting} </p>
+    </div>
+  </>
+);
